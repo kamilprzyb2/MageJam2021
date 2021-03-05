658c8e2a113551d6a2358b0bf6fe5a1e667843d2
Kamil <kprzybylski2@edu.cdv.pl>
2021-03-06 00:49:16 +0100

    Implemented drawing mechanics and very basic movement

---
+++ Assets/Scripts/DrawingManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawingManager : MonoBehaviour
+{
+    public GameObject lineTemplate;
+
+    private Line activeLine;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (activeLine == null)
+            {
+                GameObject newLine = Instantiate(lineTemplate);
+                activeLine = newLine.GetComponent<Line>();
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+        
+
+        if (activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
+            activeLine.UpdateLine(mousePos);
+        }
+
+        
+
+    }
+
+}
