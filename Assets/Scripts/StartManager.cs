64baf627f50483cdded0368537a0c52d27f08e26
Kamil <kprzybylski2@edu.cdv.pl>
2021-03-07 10:52:20 +0100

    Implemented Start Menu

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    public int FirstLevel = 1;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(FirstLevel);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
